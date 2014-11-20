204b9e5ec12c691a63e3709ef1cf7e8a0b0203ca
Trey Jackson <jacksrf@gmail.com>
2014-11-20 10:40:58 -0500

    007

---
+++ Osidebyside.js
@@ -0,0 +1,11 @@
+var word = process.argv[2];
+var i = 0;
+
+while (i < word.length){
+  if (word.charAt(i) + word.charAt(i + 1) === "oo") {
+    console.log("Agent 007");
+    i++;
+  }
+ else
+   console.log("Mission fail");
+ }
