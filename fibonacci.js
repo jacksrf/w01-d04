a41fea8810e95e8cd55f2d9589ebafdc27f04d95
Trey Jackson <jacksrf@gmail.com>
2014-11-20 14:51:20 -0500

    fib

---
+++ fibonacci.js
@@ -0,0 +1,11 @@
+var userNum = process.argv[2];
+var i;
+var fib = []; //initializing array;
+
+fib[0] = 0
+fib[1] = 1
+
+for (i = 2; i< userNum; i++) {
+	fib[i] = fib[i-2] + fib[i-1]; //
+}
+console.log(fib);
